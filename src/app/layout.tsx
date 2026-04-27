8ca5766edb389dbf5bb507eba196062f7eecdc7a
Apirak Prapeso <YOUR_EMAIL@example.com>
2026-04-27 18:00:21 +0700

    fix: suppress theme hydration warning

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,7 +11,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="th">
+    <html lang="th" suppressHydrationWarning>
       <head>
         <script
           dangerouslySetInnerHTML={{
